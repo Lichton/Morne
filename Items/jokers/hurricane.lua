b3b133cc8bac51c59c71dc88848d5d3a932c6261
Photoshoot <69369461+Lichton@users.noreply.github.com>
2026-06-22 16:04:52 -0400

    fixes hurricane

--- Items/jokers/hurricane.lua
+++ Items/jokers/hurricane.lua
@@ -21,13 +21,14 @@ SMODS.Joker {
 	calculate = function(self, card, context)
 		 if context.repetition and context.cardarea == G.play then
             local retriggernum = 0
-            for _, card_in_hand in ipairs(G.hand) do
+            for _, card_in_hand in ipairs(G.hand.cards) do
+                print('hi')
                 if not SMODS.has_no_rank(card_in_hand) and card_in_hand:get_id() == 6 then
                     retriggernum = retriggernum + 1
                 end
             end
             if not SMODS.has_no_rank(context.other_card) then
-                if context.other_card:get_id() ~= 6 and retriggernum > 0 then
+                if context.other_card:get_id() == 6 and retriggernum > 0 then
                     return {
                         message = localize('k_again_ex'),
                         repetitions = retriggernum,
